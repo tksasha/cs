56d08636086beedf7447a9c55c2af02110334717
Oleksandr Tkachuk <tksasha.me@gmail.com>
2026-04-21 22:01:36 +0300

    removed unused

--- Be/Users/Repository.cs
+++ Be/Users/Repository.cs
@@ -6,11 +6,11 @@ public interface IRepository
 {
     Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken);
 
-    Task<bool> CreateAsync(User user, CancellationToken cancellationToken);
+    Task CreateAsync(User user, CancellationToken cancellationToken);
 
     Task<User?> GetByIdAsync(int id, CancellationToken cancellationToken);
 
-    Task<bool> UpdateAsync(CancellationToken cancellationToken);
+    Task UpdateAsync(CancellationToken cancellationToken);
 }
 
 public class Repository(DatabaseContext databaseContext) : IRepository
@@ -18,16 +18,16 @@ public class Repository(DatabaseContext databaseContext) : IRepository
     public async Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken)
         => await databaseContext.Users.AsNoTracking().ToListAsync(cancellationToken);
 
-    public async Task<bool> CreateAsync(User user, CancellationToken cancellationToken)
+    public async Task CreateAsync(User user, CancellationToken cancellationToken)
     {
         databaseContext.Users.Add(user);
 
-        return await databaseContext.SaveChangesAsync(cancellationToken) > 0;
+        await databaseContext.SaveChangesAsync(cancellationToken);
     }
 
     public async Task<User?> GetByIdAsync(int id, CancellationToken cancellationToken)
         => databaseContext.Users.FirstOrDefault(u => u.Id == id);
 
-    public async Task<bool> UpdateAsync(CancellationToken cancellationToken)
-        => await databaseContext.SaveChangesAsync(cancellationToken) > 0;
+    public async Task UpdateAsync(CancellationToken cancellationToken)
+        => await databaseContext.SaveChangesAsync(cancellationToken);
 }
