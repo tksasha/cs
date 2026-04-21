352c24620e8c4d8764c3c5c9eb9ce20e60dc3264
Oleksandr Tkachuk <tksasha.me@gmail.com>
2026-04-21 12:16:01 +0300

    removed unused

--- Be/Program.cs
+++ Be/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddScoped<Be.User.IService, Be.User.Service>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -24,28 +23,4 @@ if (app.Environment.IsProduction())
 app.MapGet("/users", (Be.User.IService service, CancellationToken cancellationToken)
     => service.GetUsers(cancellationToken)).WithName("GetPeople");
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast = Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast");
-
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
