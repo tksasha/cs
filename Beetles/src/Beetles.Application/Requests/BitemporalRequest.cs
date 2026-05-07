7571408517d4fc9e7ce4e4c91a50a502a1f2d79f
Oleksandr Tkachuk <tksasha.me@gmail.com>
2026-05-07 10:41:54 +0300

    removed unused

--- Beetles/src/Beetles.Application/Requests/BitemporalRequest.cs
+++ Beetles/src/Beetles.Application/Requests/BitemporalRequest.cs
@@ -3,5 +3,4 @@ namespace Beetles.Application.Requests;
 public abstract record class BitemporalRequest
 {
     public DateTimeOffset BusinessStart { get; set; }
-    public DateTimeOffset? BusinessEnd { get; set; }
 }
--- Beetles/src/Beetles.Application/Requests/WallRequest.cs
+++ Beetles/src/Beetles.Application/Requests/WallRequest.cs
@@ -7,5 +7,5 @@ public sealed record class WallRequest : BitemporalRequest
     public required string Color { get; set; }
 
     public Wall ToEntity()
-        => new() { Color = Color, BusinessStart = BusinessStart, BusinessEnd = BusinessEnd };
+        => new() { Color = Color, BusinessStart = BusinessStart };
 }
